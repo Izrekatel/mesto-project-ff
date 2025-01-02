93e6b5cf51d3b9aa7f8a9e39e198baa2eb79c03a
Izrekatel <gelrudboris@gmail.com>
2025-01-02 14:51:31 +0500

    add popups

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,6 +3,12 @@ import '.././styles/index.css';
 
 const container = document.querySelector('.content');
 const cardContainer = container.querySelector('.places__list');
+const profileEditButton = container.querySelector('.profile__edit-button');
+const popupEditButton = document.querySelector('.popup_type_edit');
+const profileAddButton = container.querySelector('.profile__add-button');
+const popupNewCardButton = document.querySelector('.popup_type_new-card');
+const popupCardImage = document.querySelector('.popup_type_image');
+
 
 function addCardTemplateClone() {
     const cardTemplate = document.querySelector('#card-template').content;
@@ -14,9 +20,22 @@ function deleteCard(cardElement) {
     cardElement.remove();
 }
 
+function showPopup(popup, cardImage='') {
+    if (cardImage) {
+        const popupImg = popup.querySelector('.popup__image');
+        popupImg.src = cardImage.src;
+        popupImg.alt = cardImage.alt;
+    }
+    popup.classList.toggle('popup_is-opened');
+}
+
 function addCardDeleteButtonListener(cardElement) {
     const cardDeleteButton = cardElement.querySelector('.card__delete-button');
     cardDeleteButton.addEventListener('click', () => deleteCard(cardElement));
+}
+
+function addCardImageClickListener(cardImage) {
+    cardImage.addEventListener('click', () => showPopup(popupCardImage, cardImage));
 }
 
 function fillCard(element) {
@@ -26,6 +45,7 @@ function fillCard(element) {
     cardImage.alt = element.name;
     cardElement.querySelector('.card__title').textContent = element.name;
     addCardDeleteButtonListener(cardElement);
+    addCardImageClickListener(cardImage);
     return cardElement;
 }
 
@@ -38,3 +58,6 @@ initialCards.forEach(function (element) {
     const createdCard = fillCard(element);
     addCard(createdCard);
 });
+
+profileEditButton.addEventListener('click', () => showPopup(popupEditButton));
+profileAddButton.addEventListener('click', () => showPopup(popupNewCardButton));
